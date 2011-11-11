0ab2e55767981665b6cf7ece5f78345780e959fa
Bill Caputo <bcaputo@drw.com>
2011-11-11 18:27:50 -0500

    factored out key

--- count.clj
+++ count.clj
@@ -1,5 +1,5 @@
 (defn count-stream [msg]
-  (let [old-val (read-value "foo")]
-  (if-let [n (get msg "foo")]
-    (update-value "foo" (+ (if old-val old-val 0) n)))))
+  (let [k "foo" old-val (read-value k)]
+  (if-let [n (get msg k)]
+    (update-value k (+ (if old-val old-val 0) n)))))
 
