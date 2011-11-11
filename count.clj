5fd6e386b2048a4a7902f6784c6eb4e2ad27d7ec
Bill Caputo <bcaputo@drw.com>
2011-11-11 18:21:50 -0500

    handle missing val in storage

--- count.clj
+++ count.clj
@@ -1,4 +1,5 @@
 (defn count-stream [msg]
+  (let [old-val (read-value "foo")]
   (if-let [n (get msg "foo")]
-    (update-value "foo" (+ (read-value "foo") n))))
+    (update-value "foo" (+ (if old-val old-val 0) n)))))
 
